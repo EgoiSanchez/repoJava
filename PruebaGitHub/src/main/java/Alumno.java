9bdc92b96597edd5b76695456d47f3891414d9b9
Egoi <egoi.sanchez@ikasle.egibide.org>
2024-04-12 21:30:42 +0200

    Atributos alumno

--- PruebaGitHub/src/main/java/Alumno.java
+++ PruebaGitHub/src/main/java/Alumno.java
@@ -1,2 +1,12 @@
 public class Alumno {
+
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
